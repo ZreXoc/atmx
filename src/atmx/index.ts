8186918f0783ad5e21b5c2d1c1dc38fa9340f234
ZreXoc <ZeeXoc@outlook.com>
2021-08-10 00:11:10 +0800

    api rewrited

---
+++ src/atmx/index.ts
@@ -0,0 +1,17 @@
+export * from './components/EditArea';
+export * from './components/MainEditor';
+export * from './components/PropertiesPanel';
+export * from './components/ToolBar';
+export * from './hooks/useEditorConfig';
+export * from './hooks/useEditorValue';
+export * from './interfaces/render';
+export * from './interfaces/type';
+export * from './utils/render';
+export * from './utils/serialize';
+export * from './utils/EditorInitializer';
+export * as TextCommand from './utils/TextCommand';
+
+
+
+
+
