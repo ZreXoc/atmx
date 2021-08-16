3793b4082a1d0f6c33057961549abd9b0f538530
ZreXoc <ZeeXoc@outlook.com>
2021-08-16 16:31:18 +0800

    now can pass attritube to serilizer and renderer

--- src/atmx/components/ToolBar.tsx
+++ src/atmx/components/ToolBar.tsx
@@ -1,7 +1,15 @@
-import { Button as AntdButton, ButtonProps, Divider, Dropdown, Menu, Modal, Space } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import { Button as AntdButton, ButtonProps, Divider, Dropdown, Menu, Modal, Space, Upload, message } from "antd";
+import { DraggerProps, UploadChangeParam } from "antd/lib/upload";
+import { UploadFile } from "antd/lib/upload/interface";
 import ClipboardJS from "clipboard";
-
+import { saveAs } from 'file-saver';
+import { useCallback } from "react";
+import ReactDOM from "react-dom";
+import { Descendant, Editor, Transforms } from "slate";
+import { useSlate } from "slate-react";
 import { AchievedNode, BlockNode, InlineNode, TextCommand as command, useEditorInfo } from "..";
+const { Dragger } = Upload;
 
 const ToolBar: React.FC = props => {
     const { editor } = useEditorInfo();
@@ -31,18 +39,18 @@ export const DefaultButton: React.FC<{ isActive?: boolean } & ButtonProps> = (pr
     )
 }
 
-export const InlineButton: React.FC<{ format: InlineNode & AchievedNode, icon?: React.ReactNode } & ButtonProps> = (props) => {
-    const { editor } = useEditorInfo()
-    const { format, icon, ...buttonProps } = props
+export const InlineButton: React.FC<{ format: InlineNode & AchievedNode, attr?: Object, isActive?: (editor: Editor) => boolean, icon?: React.ReactNode } & ButtonProps> = (props) => {
+    const editor = useSlate();
+    const { format, attr = {}, icon, ...buttonProps } = props
 
     return (
         <AntdButton size='small' type="text" icon={icon} title={format.title}
             style={{
-                color: command.isMarkActive(format.key) ? "#1890ff" : undefined
+                color: (format.isActive ? format.isActive(editor, attr) : command.isBlockActive(format.key)) ? "#1890ff" : undefined
             }}
             onClick={e => {
                 e.preventDefault()
-                format.achieve(editor);
+                format.achieve(editor, attr || {});
             }}
             {...buttonProps}
         >
@@ -50,20 +58,21 @@ export const InlineButton: React.FC<{ format: InlineNode & AchievedNode, icon?: 
         </AntdButton>
     )
 }
-export const BlockButton: React.FC<{ format: BlockNode & AchievedNode, icon?: React.ReactNode } & ButtonProps> = (props) => {
-    const { editor } = useEditorInfo();
-    const { format, icon } = props;
+export const BlockButton: React.FC<{ format: BlockNode & AchievedNode, attr?: Object, icon?: React.ReactNode } & ButtonProps> = (props) => {
+    const editor = useSlate();
+    const { format, attr = {}, icon, ...buttonProps } = props;
 
     return (
         <AntdButton size='small' style={{
-            color: command.isBlockActive(format.key) ? "#1890ff" : undefined
+            color: (format.isActive ? format.isActive(editor, attr) : command.isBlockActive(format.key)) ? "#1890ff" : undefined
         }}
             type="text"
             icon={icon}
             onClick={e => {
                 e.preventDefault();
-                format.achieve(editor);
+                format.achieve(editor, attr);
             }}
+            {...buttonProps}
         >
             {props.children}
         </AntdButton>
@@ -71,13 +80,63 @@ export const BlockButton: React.FC<{ format: BlockNode & AchievedNode, icon?: Re
 }
 
 export const StartMenu = () => {
-    const { serialize } = useEditorInfo();
+    const { editor, initialValue, serialize } = useEditorInfo();
     return <div className='header'>
         <Dropdown.Button size='small' type='ghost' trigger={["hover"]}
             overlay={
                 <Menu onClick={
                     ev => {
                         switch (ev.key) {
+                            case 'new':
+                                Modal.confirm({
+                                    title: '新建',
+                                    onOk: () => {
+                                        localStorage.removeItem('content');
+                                        window.location.reload();
+                                    },
+                                    content:
+                                        <>
+                                            您当前的文本会被清除且无法恢复,请确保当前文本已保存或导出
+                                        </>,
+                                });
+                                break;
+                            case 'open':
+                                const reader = new FileReader();
+                                let file: Blob;
+                                reader.onload = (evt) => {
+                                    if (!evt.target || typeof evt.target.result !== 'string') return message.error(`打开失败`);
+                                    message.success(`已打开`);
+                                    //editor.children = JSON.parse(evt.target.result) as Descendant[];
+                                    localStorage.setItem('content', evt.target.result);
+                                    window.location.reload();
+                                };
+                                const props: DraggerProps = {
+                                    name: 'file',
+                                    multiple: false,
+                                    beforeUpload: (f) => {
+                                        file = f;
+                                        return false;
+                                    }
+                                };
+                                Modal.confirm({
+                                    title: '打开文件',
+                                    onOk: () => {
+                                        reader.readAsText(file);
+                                    },
+                                    content:
+                                        <>
+                                            <Dragger {...props}>
+                                                <p className="ant-upload-drag-icon">
+                                                    <InboxOutlined />
+                                                </p>
+                                                <p className="ant-upload-text">点击或拖延文件到此处以打开</p>
+                                                <p className="ant-upload-hint">
+                                                    打开操作会覆盖您当前的文本，请确保当前文本已保存或导出
+                                                </p>
+                                            </Dragger>
+                                        </>,
+                                });
+                                break;
                             case 'export':
                                 Modal.confirm({
                                     title: '导出为wikidot',
@@ -87,13 +146,17 @@ export const StartMenu = () => {
                                             <AntdButton id='export-copy' data-clipboard-action="copy" data-clipboard-target="#export-output">copy</AntdButton>
                                         </>,
                                 });
+                                break;
+                            case 'save':
+                                saveAs(new Blob([JSON.stringify(initialValue)], { type: 'application/json' }), "atmx.json");
+                                break;
                         }
                     }
                 }>
-                    <Menu.Item
-                        key='export'
-                    >导出
-                    </Menu.Item>
+                    <Menu.Item key='new'>新建</Menu.Item>
+                    <Menu.Item key='open'>打开</Menu.Item>
+                    <Menu.Item key='export'>导出</Menu.Item>
+                    <Menu.Item key='save'>保存到本地</Menu.Item>
                 </Menu>
             }>开始</Dropdown.Button>
     </div>
